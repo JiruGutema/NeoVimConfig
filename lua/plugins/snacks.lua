19d0c179f5d2fc904f522d470b89d052342f5fc2
JiruGutema <jirudagutema@gmail.com>
2025-12-04 21:42:56 +0300

    refactor: refactor unecessary config in the snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,11 +6,6 @@ return {
         preset = "ivy",
         hidden = { "input" },
       },
-    },
-    explorer = {
-      layout = {
-        hidden = { "input" },
-      },
-    },
+    }
   },
 }
